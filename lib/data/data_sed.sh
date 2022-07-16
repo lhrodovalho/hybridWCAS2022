f7cb8a9d5bafa189349dabc1b9f6a455dff88f01
Luis Henrique Rodovalho <luishenriquerodovalho@gmail.com>
2022-07-16 13:00:50 +0100

    Uploaded initial files

---
+++ lib/data/data_sed.sh
@@ -0,0 +1,9 @@
+sed 's/^/ /; s/  /,/g; s/ /,/g; s/^,//g' ac_df_av.txt > ac_df_av.csv
+sed 's/^/ /; s/  /,/g; s/ /,/g; s/^,//g' ac_df_ph.txt > ac_df_ph.csv
+sed 's/^/ /; s/  /,/g; s/ /,/g; s/^,//g' ac_cm_av.txt > ac_cm_av.csv
+
+sed 's/^/ /; s/  /,/g; s/ /,/g; s/^,//g' dc_df_av.txt > dc_df_av.csv
+sed 's/^/ /; s/  /,/g; s/ /,/g; s/^,//g' dc_df_vo.txt > dc_df_vo.csv
+sed 's/^/ /; s/  /,/g; s/ /,/g; s/^,//g' dc_cm_av.txt > dc_cm_av.csv
+sed 's/^/ /; s/  /,/g; s/ /,/g; s/^,//g' dc_cm_vo.txt > dc_cm_vo.csv
+
